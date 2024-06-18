84fa7c74ba4fd17080dc6f388dd67ce2f4631a81
Maksim Nahirniy <maksym.nahirnyi@dexola.com>
2024-06-18 15:12:02 +0300

    FIX | bug with convert WETH to ETH during selling

--- contracts/BiscuitV1.sol
+++ contracts/BiscuitV1.sol
@@ -3,13 +3,14 @@ pragma solidity ^0.8.24;
 
 import {IUniswapV3Factory} from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
 import {IV3SwapRouter} from "@uniswap/swap-router-contracts/contracts/interfaces/IV3SwapRouter.sol";
-import {IPeripheryPayments} from "@uniswap/v3-periphery/contracts/interfaces/IPeripheryPayments.sol";
+import {IWETH} from "@uniswap/swap-router-contracts/contracts/interfaces/IWETH.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {OracleLibrary} from "./libraries/OracleLibrary.sol";
+import "hardhat/console.sol";
 
 error NotContract(address account);
 error ValueUnchanged();
@@ -21,6 +22,7 @@ error NotApprovedOrOwner();
 error MixedPaymentNotAllowed();
 error PaymentAmountZero();
 error WithdrawFailed();
+error ETHTransferFailed();
 
 contract BiscuitV1 is ERC721, AccessControl {
     using SafeERC20 for IERC20;
@@ -43,7 +45,7 @@ contract BiscuitV1 is ERC721, AccessControl {
     IUniswapV3Factory public immutable UNISWAP_FACTORY;
     IV3SwapRouter public immutable SWAP_ROUTER;
     IERC20 public immutable TOKEN;
-    IERC20 public immutable WETH;
+    IWETH public immutable WETH;
 
     uint256 public constant BIPS = 100_00;
     uint256 public constant SLIPPAGE_MULTIPLIER = BIPS - 5_00;
@@ -84,7 +86,7 @@ contract BiscuitV1 is ERC721, AccessControl {
         UNISWAP_FACTORY = IUniswapV3Factory(_uniswapFactory);
         SWAP_ROUTER = IV3SwapRouter(_swapRouter);
         TOKEN = IERC20(_token);
-        WETH = IERC20(_weth);
+        WETH = IWETH(_weth);
 
         _grantRole(DEFAULT_ADMIN_ROLE, _admin);
     }
@@ -100,10 +102,11 @@ contract BiscuitV1 is ERC721, AccessControl {
         if (msg.value == 0 && _amountToken == 0) revert PaymentAmountZero();
 
         address tokenIn = _amountToken > 0 ? address(TOKEN) : address(WETH);
+        uint256 amountPayment = _amountToken > 0 ? _amountToken : msg.value;
         uint256 transactionTimeout = _transactionTimeout != 0 ? _transactionTimeout : DEFAULT_TRANSACTION_TIMEOUT;
         uint24 poolFee = _poolFee != 0 ? _poolFee : DEFAULT_POOL_FEE;
 
-        _buyPortfolio(tokenIn, _portfolioId, _amountToken, transactionTimeout, poolFee);
+        _buyPortfolio(tokenIn, _portfolioId, amountPayment, transactionTimeout, poolFee);
         emit PortfolioPurchased(_portfolioId, msg.sender, _amountToken, msg.value);
     }
 
@@ -151,7 +154,7 @@ contract BiscuitV1 is ERC721, AccessControl {
     function getTokenExists(address _token) public view returns (bool) {
         address pair = UNISWAP_FACTORY.getPool(
             _token,
-            address(TOKEN),
+            address(WETH),
             DEFAULT_POOL_FEE
         );
         return pair != address(0);
@@ -254,16 +257,18 @@ contract BiscuitV1 is ERC721, AccessControl {
         uint256 _transactionTimeout,
         uint24 _poolFee
     ) private {
+        // Invested amount token or ETH that including service fee
+        uint256 investedAmount = _amountPayment * (BIPS - serviceFee) / BIPS;
         TokenShare[] memory portfolio = portfolios[_portfolioId];
         TokenAmount[] memory boughtPortfolio = new TokenAmount[](portfolio.length);
 
         if (_tokenIn == address(TOKEN)) {
             IERC20(_tokenIn).safeTransferFrom(msg.sender, address(this), _amountPayment);
-            IERC20(_tokenIn).approve(address(SWAP_ROUTER), _amountPayment);
+        } else {
+            WETH.deposit{value: investedAmount}();
         }
 
-        // Invested amount token that including service fee
-        uint256 investedAmount = _amountPayment * (BIPS - serviceFee) / BIPS;
+        IERC20(_tokenIn).approve(address(SWAP_ROUTER), investedAmount);
         for (uint256 i = 0; i < portfolio.length; i++) {
             TokenShare memory portfolioToken = portfolio[i];
 
@@ -292,15 +297,20 @@ contract BiscuitV1 is ERC721, AccessControl {
         uint256 _transactionTimeout,
         uint24 _fee
     ) private {
-        TokenAmount[] memory portfolio = purchasedPortfolios[_tokenId].portfolio;
+        PurchasedPortfolio memory purchasedPortfolio = purchasedPortfolios[_tokenId];
 
-        for (uint256 i = 0; i < portfolio.length; i++) {
-            TokenAmount memory portfolioToken = portfolio[i];
+        for (uint256 i = 0; i < purchasedPortfolio.portfolio.length; i++) {
+            TokenAmount memory portfolioToken = purchasedPortfolio.portfolio[i];
 
             IERC20(portfolioToken.token).approve(address(SWAP_ROUTER), portfolioToken.amount);
             uint256 amountOut = _swap(portfolioToken.token, _tokenOut, portfolioToken.amount, _fee);
-            // need to wrap WETH to ETH when tokenOut == WETH
-            IERC20(portfolioToken.token).safeTransferFrom(address(this), msg.sender, amountOut);
+            if (purchasedPortfolio.purchasedWithETH) {
+                WETH.withdraw(amountOut);
+                (bool success, ) = msg.sender.call{value: amountOut}("");
+                if(!success) revert ETHTransferFailed();
+            } else {
+                IERC20(portfolioToken.token).safeTransferFrom(address(this), msg.sender, amountOut);
+            }
         }
         
         delete purchasedPortfolios[_tokenId];
@@ -331,12 +341,7 @@ contract BiscuitV1 is ERC721, AccessControl {
                 sqrtPriceLimitX96: 0
             });
 
-        // TODO: need to fix bug with during selling
-        if (_tokenIn == address(WETH)) {
-            amountOut = SWAP_ROUTER.exactInputSingle{value: _amountIn}(params);
-        } else {
-            amountOut = SWAP_ROUTER.exactInputSingle(params);
-        }
+        amountOut = SWAP_ROUTER.exactInputSingle(params);
     }
 
     function _checkIsContract(address _address) private view {
@@ -374,4 +379,6 @@ contract BiscuitV1 is ERC721, AccessControl {
     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
+
+    receive() external payable {}
 }
