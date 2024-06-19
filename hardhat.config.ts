875f0b52e9f661539a4102cf80980f66bc7059b0
Maksim Nahirniy <maksym.nahirnyi@dexola.com>
2024-06-19 15:44:29 +0300

    ADD | setting for hardhat and dotenv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,31 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config'
+
+const { ETH_SEPOLIA_TESTNET_URL, ETH_MAINNET_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+    solidity: "0.8.24",
+    networks: {
+        hardhat: {
+            forking: {
+                url: ETH_SEPOLIA_TESTNET_URL || "",
+                blockNumber: 6133134,
+                enabled: true
+            },
+        },
+        mainnet: {
+            url: ETH_MAINNET_URL || "",
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+        },
+        sepolia: {
+            url: ETH_SEPOLIA_TESTNET_URL || "",
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+        }
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    }
 };
 
 export default config;
